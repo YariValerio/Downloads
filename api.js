1b194f8c087b8d808508a827c2280842bc0545ce
YariValerio <89805279+YariValerio@users.noreply.github.com>
2021-09-27 15:08:34 -0400

    lab04

---
+++ api.js
@@ -0,0 +1,21 @@
+// fetch data from a public API
+function doAPICall() {
+
+  # your fetch code here
+
+}
+
+
+// callback function to be used when data is fetched from API
+// parameter data will be a JavaScript object
+function processData(data) {
+
+  # Get the div or other DOM element needed for the data
+
+
+  # Access the necessary fields of the data
+
+
+  # Update the DOM to display the data in some way
+
+}
